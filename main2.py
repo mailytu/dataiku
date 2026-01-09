23f4c0b934c366acd859f248a27383b083db6f5e
mailytu <turluremaily2901@gmail.com>
2026-01-09 17:08:38 +0100

    Add main2.py

---
+++ main2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Nom du fichier (tel qu'il apparaît dans VS Code)
+file_path = "dataset_anonymized_flight_Sheet1 (1).xlsx"
+
+# Charger le fichier Excel
+df = pd.read_excel(file_path)
+
+# Afficher les premières lignes
+print(df.head())
